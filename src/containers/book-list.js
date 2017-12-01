71386af125e0ea247465bbf078b6a0e1984f3128
Vilde Vevatne <vvevatne@pinterest.com>
2017-11-30 17:57:50 -0800

    Add dispatch method

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 	renderList() {
@@ -24,6 +26,10 @@ function mapStateToProps(state) {
 	return {
 		books: state.books
 	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({selectBook: selectBook}, dispatch)
 }
 
 export default connect(mapStateToProps)(BookList);
